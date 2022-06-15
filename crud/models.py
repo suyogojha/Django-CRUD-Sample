e5fc7fc548cf09b02b05523789685ade25db9ba8
suyog ojha <ojhasuyog2020@gmail.com>
2022-06-15 21:32:33 +0545

    **

---
+++ crud/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=40)
+    lastname = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.firstname + " " + self.lastname
---
+++ crud/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns= [
+    url(r'^$', views.index, name='index'),
+    url(r'^create$', views.create, name='create'),
+    url(r'^edit/(?P<id>\d+)$', views.edit, name='edit'),
+    url(r'^edit/update/(?P<id>\d+)$', views.update, name='update'),
+    url(r'^delete/(?P<id>\d+)$', views.delete, name='delete'),
+]
